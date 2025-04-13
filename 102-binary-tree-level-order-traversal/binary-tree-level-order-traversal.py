6c79f4174e3ec871da40ee804e16c172958c10f6
Biswajeet <40053288+biswajeet1999@users.noreply.github.com>
2025-04-13 20:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (60.89%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,31 +5,57 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    #     if root is None:
+    #         return []
+
+    #     res = []
+    #     curLevel = []
+    #     queue = []
+
+    #     queue.append(root)
+    #     queue.append(None)
+
+    #     while len(queue) > 0:
+    #         cur = queue.pop(0)
+
+    #         if cur == None:
+    #             if len(queue) > 0:
+    #                 queue.append(None)
+    #             res.append(curLevel)
+    #             curLevel = []
+    #             continue
+    #         curLevel.append(cur.val)
+            
+    #         if cur.left:
+    #             queue.append(cur.left)
+    #         if cur.right:
+    #             queue.append(cur.right)
+
+    #     return res
+
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root is None:
             return []
-            
+
         res = []
-        curLevel = []
         queue = []
 
         queue.append(root)
-        queue.append(None)
 
         while len(queue) > 0:
-            cur = queue.pop(0)
+            nodesInCurLevel = len(queue)
+            curLevel = []
 
-            if cur == None:
-                if len(queue) > 0:
-                    queue.append(None)
-                res.append(curLevel)
-                curLevel = []
-                continue
-            curLevel.append(cur.val)
-            
-            if cur.left:
-                queue.append(cur.left)
-            if cur.right:
-                queue.append(cur.right)
+            while nodesInCurLevel > 0:
+                cur = queue.pop(0)
+                curLevel.append(cur.val)
+                nodesInCurLevel -= 1
 
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            res.append(curLevel)
+        
         return res
