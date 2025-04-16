3e0eb4f32637925fc08a3126758c19cc36471a51
Biswajeet <40053288+biswajeet1999@users.noreply.github.com>
2025-04-16 23:09:13 +0530

    Time: 8 ms (58.73%) | Memory: 18.8 MB (34.80%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
+        
+        steps = nums[0]
+        maxJump = nums[0]
+        minJump = 0
+
+        for idx in range(1, len(nums) - 1):
+            steps -= 1
+            maxJump = max(maxJump, nums[idx] + idx)
+
+            if steps == 0:
+                minJump += 1
+                steps = maxJump - idx
+        
+        return minJump + 1
+
+        
