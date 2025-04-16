29b6fca1b25dba9b1f50086f26aae5b582398db7
Biswajeet <40053288+biswajeet1999@users.noreply.github.com>
2025-04-17 00:09:54 +0530

    Time: 9 ms (7.28%) | Memory: 17.9 MB (40.50%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        n = len(s)
+        memo = [[-1] * n for _ in range(n)]
+        return self.is_valid_string(0, 0, s, memo)
+
+    def is_valid_string(self, index: int, open_count: int, s: str, memo: List[List[int]]) -> bool:
+        
+        if index == len(s):
+            return open_count == 0
+
+        
+        if memo[index][open_count] != -1:
+            return memo[index][open_count] == 1
+
+        is_valid = False
+        
+        if s[index] == '*':
+            is_valid |= self.is_valid_string(index + 1, open_count + 1, s, memo)  # Treat '*' as '('
+            if open_count > 0:
+                is_valid |= self.is_valid_string(index + 1, open_count - 1, s, memo)  # Treat '*' as ')'
+            is_valid |= self.is_valid_string(index + 1, open_count, s, memo)  # Treat '*' as empty
+        else:
+            
+            if s[index] == '(':
+                is_valid = self.is_valid_string(index + 1, open_count + 1, s, memo)  # Increment count for '('
+            elif open_count > 0:
+                is_valid = self.is_valid_string(index + 1, open_count - 1, s, memo)  # Decrement count for ')'
+
+       
+        memo[index][open_count] = 1 if is_valid else 0
+        return is_valid
